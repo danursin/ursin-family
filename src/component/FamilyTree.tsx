67f52c6f1263fa4cd07a17707c634984556dedac
danursin <danursin@gmail.com>
2020-12-02 00:03:11 -0600

    horizontal tidy-tree

--- src/component/FamilyTree.tsx
+++ src/component/FamilyTree.tsx
@@ -1,5 +1,6 @@
 import * as d3 from "d3";
 
+import { HierarchyPointNode, TreeLayout } from "d3";
 import React, { useEffect, useRef } from "react";
 
 import { Person } from "../types";
@@ -47,20 +48,41 @@ const FamilyTree: React.FC = () => {
 
         const json = assembleFamilyData(family);
         const width = 954; //document.body.clientWidth;
-        const radius = width / 2;
 
-        const tree = d3
-            .tree<Datum>()
-            .size([2 * Math.PI, radius])
-            .separation((a, b) => (a.parent === b.parent ? 1 : 2) / a.depth);
+        let dx = 10;
+        let dy = -1;
 
-        const data = d3.hierarchy<Datum>(json).sort((a, b) => d3.ascending(a.data.name, b.data.name));
+        const buildTree = (data: Datum): d3.HierarchyPointNode<Datum> => {
+            const root = d3.hierarchy<Datum>(data);
+            dx = 10;
+            dy = width / (root.height + 1);
+            return d3.tree<Datum>().nodeSize([dx, dy])(root);
+        };
 
-        const root = tree(data);
+        const root = buildTree(json);
 
-        const svg = d3.select(svgNode);
+        let x0 = Infinity;
+        let x1 = -x0;
 
-        svg.append("g")
+        root.each((d) => {
+            if (d.x > x1) {
+                x1 = d.x;
+            }
+            if (d.x < x0) {
+                x0 = d.x;
+            }
+        });
+
+        const svg = d3.select(svgNode).attr("viewBox", [0, 0, width, x1 - x0 + dx * 2].join());
+
+        const g = svg
+            .append("g")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+            .attr("transform", `translate(${dy / 3},${dx - x0})`);
+
+        const link = g
+            .append("g")
             .attr("fill", "none")
             .attr("stroke", "#555")
             .attr("stroke-opacity", 0.4)
@@ -71,57 +93,32 @@ const FamilyTree: React.FC = () => {
             .attr(
                 "d",
                 d3
-                    .linkRadial<unknown, d3.HierarchyPointNode<Datum>>()
-                    .angle((d) => d.x)
-                    .radius((d) => d.y)
+                    .linkHorizontal<unknown, HierarchyPointNode<Datum>>()
+                    .x((d) => d.y)
+                    .y((d) => d.x)
             );
 
-        svg.append("g")
-            .selectAll("circle")
+        const node = g
+            .append("g")
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-width", 3)
+            .selectAll("g")
             .data(root.descendants())
-            .join("circle")
-            .attr(
-                "transform",
-                (d) => `
-                    rotate(${(d.x * 180) / Math.PI - 90})
-                    translate(${d.y},0)
-                `
-            )
+            .join("g")
+            .attr("transform", (d) => `translate(${d.y},${d.x})`);
+
+        node.append("circle")
             .attr("fill", (d) => (d.children ? "#555" : "#999"))
             .attr("r", 2.5);
 
-        svg.append("g")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", 10)
-            .attr("stroke-linejoin", "round")
-            .attr("stroke-width", 3)
-            .selectAll("text")
-            .data(root.descendants())
-            .join("text")
-            .attr(
-                "transform",
-                (d) => `
-        rotate(${(d.x * 180) / Math.PI - 90}) 
-        translate(${d.y},0) 
-        rotate(${d.x >= Math.PI ? 180 : 0})
-      `
-            )
+        node.append("text")
             .attr("dy", "0.31em")
-            .attr("x", (d) => (d.x < Math.PI === !d.children ? 6 : -6))
-            .attr("text-anchor", (d) => (d.x < Math.PI === !d.children ? "start" : "end"))
+            .attr("x", (d) => (d.children ? -6 : 6))
+            .attr("text-anchor", (d) => (d.children ? "end" : "start"))
             .text((d) => d.data.name)
             .clone(true)
             .lower()
             .attr("stroke", "white");
-
-        svg.attr("viewBox", () => {
-            const _this = svg.node();
-            if (!_this) {
-                return null;
-            }
-            const { x, y, width, height } = _this.getBBox();
-            return [x, y, width, height].join();
-        });
     }, []);
 
     return <svg ref={svgRef} />;
