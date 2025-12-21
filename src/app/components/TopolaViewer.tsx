5bb6829b499843dc568b40e872bc6f11d3315b4e
danursin <danursin@gmail.com>
2025-12-20 19:50:25 -0600

    add url to API

---
+++ src/app/components/TopolaViewer.tsx
@@ -0,0 +1,8 @@
+export default function TopolaViewer() {
+    return (
+        <iframe
+            style={{ width: "100%", height: "90vh", margin: 0 }}
+            src="https://topolaproxy.bieda.it/https://ursin-family.vercel.app/api"
+        ></iframe>
+    );
+}
