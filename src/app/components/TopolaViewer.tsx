3d5a74df3aae070ba9d829dfd69d90fa50fd02d6
danursin <danursin@gmail.com>
2025-12-21 11:32:04 -0600

    move route to /api/gedcom

--- src/app/components/TopolaViewer.tsx
+++ src/app/components/TopolaViewer.tsx
@@ -1,6 +1,6 @@
 // a React component that uses an iframe
 export default function TopolaViewer() {
-    const gedcomUrl = "https://ursin-family.vercel.app/api"; // your API endpoint
+    const gedcomUrl = "https://ursin-family.vercel.app/api/gedcom";
     const src = `https://pewu.github.io/topola-viewer/#/view?url=${encodeURIComponent(gedcomUrl)}&embedded=true`;
     return <iframe src={src} style={{ width: "100%", height: "90vh", border: "0" }} allowFullScreen />;
 }
