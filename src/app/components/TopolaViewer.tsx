4ee6819e95066c123e1839803542cb38044925c8
danursin <danursin@gmail.com>
2026-01-03 23:39:44 -0600

    try fullscreen

--- src/app/components/TopolaViewer.tsx
+++ src/app/components/TopolaViewer.tsx
@@ -1,10 +1,28 @@
-// a React component that uses an iframe
+"use client";
+
+import React from "react";
+
+const gedcomUrl = "https://ursin-family.vercel.app/api/gedcom";
+const params = new URLSearchParams({
+    url: gedcomUrl,
+    embedded: "true"
+});
+const src = `https://pewu.github.io/topola-viewer/#/view?${params.toString()}`;
+
 export default function TopolaViewer() {
-    const gedcomUrl = "https://ursin-family.vercel.app/api/gedcom";
-    const params = new URLSearchParams({
-        url: gedcomUrl,
-        embedded: "true"
-    });
-    const src = `https://pewu.github.io/topola-viewer/#/view?${params.toString()}`;
-    return <iframe src={src} style={{ width: "100%", height: "85vh", border: "0" }} allowFullScreen />;
+    const iframeRef = React.useRef<HTMLIFrameElement | null>(null);
+
+    const enterFullscreen = async () => {
+        const current = iframeRef.current;
+        if (current) {
+            await current.requestFullscreen();
+        }
+    };
+
+    return (
+        <>
+            <button onClick={enterFullscreen}>Full Screen</button>
+            <iframe src={src} style={{ width: "100%", height: "85vh", border: "0" }} allowFullScreen />
+        </>
+    );
 }
